d43a79e44a23c4a8b7ce72b5a5bfb0ae5a04b7cb
phamhoangminh2002 <phamhoangminh2002@gmail.com>
2023-04-10 20:55:19 +0700

    ..

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
+  axios.defaults.withCredentials = true;
+  axios.defaults.baseURL = "https://tiktok-clone-api.herokuapp.com";
+  // axios.defaults.baseURL = 'http://localhost:8000'
+  axios.defaults.headers.post["Content-Type"] = "application/json";
+  axios.defaults.headers.post["Accept"] = "application/json";
+  axios.defaults.withCredentials = true;
 
-    axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'https://tiktok-clone-api.herokuapp.com'
-    // axios.defaults.baseURL = 'http://localhost:8000'
-    axios.defaults.withCredentials = true;
-
-    return {
-        provide: { 
-            axios: axios
-        },
-    }
-})
+  return {
+    provide: {
+      axios: axios,
+    },
+  };
+});
